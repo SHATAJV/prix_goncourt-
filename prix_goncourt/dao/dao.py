6bc70c66cd0d88f3c7d3db3a7b9b3cc61e2870b7
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-17 14:04:53 +0200

    add function get_db

---
+++ prix_goncourt/dao/dao.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe abstraite générique Dao[T], dont hérite les classes de DAO de chaque entité
+"""
+
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from typing import ClassVar, Optional
+import pymysql.cursors
+
+
+# ----- Database Connection -----
+def get_db_connection():
+    connection = pymysql.connector.connect(
+        host="localhost",
+        user="prix_goncourt",
+        password="prix*goncourt2024",
+        database="prix_goncourt"
+    )
+    return connection
