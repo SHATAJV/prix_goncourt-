b56f9296f7cadb99f2c4dca7ec094f180971b27a
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-18 10:26:45 +0200

    add more function

--- prix_goncourt/models/selection.py
+++ prix_goncourt/models/selection.py
@@ -6,5 +6,10 @@ class Selection:
      def __init__(self, selection_number):
          self.selection_number= selection_number
          self.books = []
-     def __str__(self):
-         return f"this is selection number{self.selection_number} est these are the book whitch is selected{self.book}"
+     def add_book (self, book):
+         self.books.append(book)
+     def display_book(self):
+         print(f"this is selection number{self.selection_number}")
+         for book in self.books:
+          print(f"list of books{book.title}")
+
