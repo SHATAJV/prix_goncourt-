f85d0c1eb5cf49718b02121d7b60346e89facec3
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-18 10:11:32 +0200

    add modification

--- prix_goncourt/models/president.py
+++ prix_goncourt/models/president.py
@@ -5,17 +5,17 @@ class President(Members):
     def __init__(self, name, password):
         super().__init__(name, password)
 
-    def manage_selections(self, id_selection, books):
+    def manage_selections(self, selection_tour, selection):
 
-        print(f"Managing selections for stage {id_selection}:")
-        for book in books:
-            print(f" - {book}")
+        print(f"Managing selections for tour {selection_tour}:")
+        for book in selection.books:
+            print(f" - {book.title}")
 
     def result(self, book_votes):
             print("result:")
             winner = max(book_votes, key=book_votes.get)
-            for book, votes in book_votes.items():
-                print(f"{book}: {votes} votes")
+            for book, vote_count in book_votes.items():
+                print(f"{book}: has {vote_count} votes")
             print(f"The winner is {winner}!")
 
 
