eb401545fbb8d3bcfd529ed141b74f9bafb86ba0
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-18 10:03:34 +0200

    add modification

--- prix_goncourt/dao/book_dao.py
+++ prix_goncourt/dao/book_dao.py
@@ -1,18 +1,22 @@
-class BookDAO:
-    def __init__(self, db_connection):
-        self.db_connection = db_connection
+# -*- coding: utf-8 -*-
+from dao.dao import get_db_connection
 
-    def get_books_by_selection(self, id_selection):
-        cursor = self.db_connection.cursor()
-        query = "SELECT title, author, publisher FROM books WHERE id_selection = %s"
-        cursor.execute(query, (id_selection,))
+
+class BookDAO:
+
+    def read_book_selected(selection_number):
+        connection= get_db_connection()
+        cursor = connection.cursor()
+        query = "SELECT b.title, b.summary, a.name, b.editor, b.publication_date, b.pages, b.isbn, b.price FROM books b JOIN JOIN authors a ON b.id_author = a.id_author WHERE s.selection_number = %s"
+        cursor.execute(query, (selection_number))
         books = cursor.fetchall()
         cursor.close()
         return books
 
-    def add_vote(self, book_id, jury_id):
-        cursor = self.db_connection.cursor()
-        query = "INSERT INTO votes (book_id, jury_id) VALUES (%s, %s)"
-        cursor.execute(query, (book_id, jury_id))
-        self.db_connection.commit()
+    def add_vote(id_book, id_jury):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        query = "INSERT INTO votes (id_book, id_jury) VALUES (%s, %s)"
+        cursor.execute(query, (id_book, id_jury))
+        connection.commit()
         cursor.close()
