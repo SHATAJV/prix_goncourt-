7649ff4fdd4c4f4d9c0f5d3812dd368bdb3a018a
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-18 10:28:13 +0200

    add more correction

--- prix_goncourt/models/selection.py
+++ prix_goncourt/models/selection.py
@@ -3,11 +3,14 @@ from models.book import Book
 
 
 class Selection:
+
      def __init__(self, selection_number):
          self.selection_number= selection_number
          self.books = []
+
      def add_book (self, book):
          self.books.append(book)
+
      def display_book(self):
          print(f"this is selection number{self.selection_number}")
          for book in self.books:
