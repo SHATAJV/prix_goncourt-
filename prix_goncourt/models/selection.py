b1177af5f4c86922935682186211ae7dad142ae4
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-18 10:20:36 +0200

    add modification

--- prix_goncourt/models/selection.py
+++ prix_goncourt/models/selection.py
@@ -3,8 +3,8 @@ from models.book import Book
 
 
 class Selection:
-     def __init__(self, selection_number, book:Book):
+     def __init__(self, selection_number):
          self.selection_number= selection_number
-         self.book = book
+         self.books = []
      def __str__(self):
          return f"this is selection number{self.selection_number} est these are the book whitch is selected{self.book}"
