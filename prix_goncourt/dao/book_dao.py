1dee27d3766929b12e6fbbd4d5b80239b24c3678
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-09-18 14:57:37 +0200

    add all menu

--- prix_goncourt/dao/book_dao.py
+++ prix_goncourt/dao/book_dao.py
@@ -1,5 +1,3 @@
-
-# dao/book_dao.py
 from dao.connection import get_db_connection
 
 class BookDAO:
@@ -11,7 +9,7 @@ class BookDAO:
             return None
         cursor = connection.cursor()
         query = """
-        SELECT b.title, b.summary, a.name AS author, b.editor, b.publication_date, b.pages, b.isbn, b.price 
+        SELECT b.id_book, b.title, b.summary, a.name AS author, b.editor, b.publication_date, b.pages, b.isbn, b.price 
         FROM books b 
         JOIN authors a ON b.id_author = a.id_author 
         JOIN selections s ON b.id_selection = s.id_selection 
@@ -30,8 +28,20 @@ class BookDAO:
             print("Failed to connect to the database.")
             return
         cursor = connection.cursor()
-        query = "INSERT INTO votes (id_book, id_jury) VALUES (%s, %s)"
-        cursor.execute(query, (id_book, id_jury))
-        connection.commit()
+
+        # Vérifie si le jury a déjà voté pour ce livre
+        check_query = "SELECT * FROM votes WHERE id_book = %s AND id_jury = %s"
+        cursor.execute(check_query, (id_book, id_jury))
+        existing_vote = cursor.fetchone()
+
+        if existing_vote:
+            print("Vous avez déjà voté pour ce livre.")
+        else:
+            # Insère un nouveau vote dans la table
+            insert_query = "INSERT INTO votes (id_book, id_jury, votes_count) VALUES (%s, %s, 1)"
+            cursor.execute(insert_query, (id_book, id_jury))
+            connection.commit()
+            print("Votre vote a été enregistré avec succès.")
+
         cursor.close()
         connection.close()
